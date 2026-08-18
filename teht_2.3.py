7c54ecb5c2d3e504fc049aa15dc1c898f7751cf0
Rajan <Rajanmuhammad16@gmail.com>
2026-08-18 15:41:22 +0300

    2.3 tehtävä tehty, suorakulmion piiri

--- teht_2.3.py
+++ teht_2.3.py
@@ -1,1 +1,13 @@
+# Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden.
+# Ohjelma tulostaa suorakulmion piirin ja pinta-alan.
+# Suorakulmion piiri tarkoittaa sen neljän sivun yhteispituutta.
+
+korkeus = float(input("mikä on suorakulmion kanta ja korkeus?"))
+kanta = float(input("mikä on suorakaiteen kanta"))
+
+A = kanta * korkeus
+piiri = kanta + korkeus + kanta + korkeus
+
+print(f"pinta ala on:", A)
+print(f"kanta ja korkeus on:", piiri)
 

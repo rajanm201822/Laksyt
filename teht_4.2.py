4a6c65c5469ee484a7742ce97b1ff3d217fea089
Rajan <Rajanmuhammad16@gmail.com>
2026-08-27 19:45:09 +0300

    4.1

---
+++ teht_4.2.py
@@ -0,0 +1,7 @@
+tuumat = float(input("Anna tuumamäärä: "))
+
+while tuumat >= 0:
+    senttimetrit = tuumat * 2.54
+    print("Senttimetreinä:", senttimetrit)
+
+    tuumat = float(input("Anna tuumamäärä: "))

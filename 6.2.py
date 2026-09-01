014229d68f60dd90469913d589569492aa5aa6a7
Ce PC <rajanmuhammad16@fgmail.com>
2026-09-01 20:27:25 +0300

    6.2

---
+++ 6.2.py
@@ -0,0 +1,14 @@
+import random
+
+def heita_noppaa(tahkot):
+    return random.randint(1, tahkot)
+
+
+maksimi = int(input("Anna nopan tahkojen määrä: "))
+
+while True:
+    silmaluku = heita_noppaa(maksimi)
+    print(silmaluku)
+
+    if silmaluku == maksimi:
+        break

ea16496bd2453cafb6f051064dd460ea73d72cd7
Ce PC <rajanmuhammad16@fgmail.com>
2026-09-05 22:09:05 +0300

    7.1

---
+++ teht_7.1.py
@@ -0,0 +1,8 @@
+vuodenajat = ("talvi", "talvi", "kevät",
+              "kevät", "kevät", "kesä",
+              "kesä", "kesä", "syksy",
+              "syksy", "syksy", "talvi")
+
+kuukausi = int(input("Anna kuukauden numero: "))
+
+print(vuodenajat[kuukausi - 1])
